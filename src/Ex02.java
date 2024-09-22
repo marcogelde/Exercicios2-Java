b9bf238ce75efd84e9640131bbc7fcb14ab77272
Marco Antonio Gelde <marcogelde@hotmail.com>
2024-09-21 22:26:01 -0300

    Documentação

--- src/Ex02.java
+++ src/Ex02.java
@@ -2,11 +2,15 @@ import java.util.Scanner;
 
 public class Ex02 {
     public static void main(String[] args) {
+
+        // Inicializando
         Scanner sc = new Scanner(System.in);
         System.out.println("Digite um numero: ");
         int n1 = sc.nextInt();
         System.out.println("Digite outro numero: ");
         int n2 = sc.nextInt();
+
+        // Comparando os dois números
         if (n1 > n2) {
             System.out.println("O número maior é: " + n1);
         } else if (n1 < n2) {
